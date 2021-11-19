eb2bb5950e2bb55afde464ed1b5f036590ac3e37
abigmiu <1552487872@qq.com>
2021-11-19 22:40:01 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,10 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <miu-button bg="linear-gradient(to left,#fa6c9f 0%,#ffe140 80%,#ffe140 80%)">
+        默认按钮
+    </miu-button>
   </div>
-  <router-view />
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script lang="ts" setup>
+import miuButton from '@/components/button'
+</script>
---
+++ src/components/button/button.vue
@@ -0,0 +1,61 @@
+<template>
+    <button :class="btnClass" :style="btnStyle">
+        <slot name="left"></slot>
+        <span><slot></slot></span>
+        <slot name="right"></slot>
+    </button>
+</template>
+
+<script lang="ts" setup>
+import { computed, defineProps } from "vue";
+
+const props = defineProps({
+    bg: {
+        type: String,
+        default: "#f7f7f7",
+    },
+    color: {
+        type: String,
+        default: "#606266",
+    },
+});
+
+const btnClass = computed(() => {
+    return {
+        "el-btn": true,
+    };
+});
+
+const btnStyle = computed(() => {
+    return {
+        background: props.bg,
+    };
+});
+</script>
+
+<style lang="scss">
+.el-btn {
+    padding: 8px 16px;
+    color: #ffffff;
+    line-height: 1;
+    outline: none;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: all 0.2s;
+    position: relative;
+    user-select: none;
+
+    &::after {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        background: inherit;
+        width: 100%;
+        height: 100%;
+        top: 2px;
+        left: 2px;
+        filter: blur(3px);
+    }
+}
+</style>
